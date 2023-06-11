242bf0279a0b83c83cbceadb1bd5e3738a6a33fe
Jonathan Calabrese <Yonibrese@gmail.com>
2023-06-11 18:41:48 +0300

    created basic get request to get product data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,14 @@
+import json
 from django.http import JsonResponse
+from products.models import products
 
 def api_home(request, *arg, **kwarg):
-    return JsonResponse({"message":"Hello world from the DJANGO API"})
+    model_data = products.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data["id"] = model_data.id
+        data["title"] = model_data.title
+        data["content"] = model_data.content
+        data["price"] = model_data.price
+    
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,5 +1,7 @@
 import requests
 
 endpoint = "http://localhost:8000/api/"
-get_response = requests.get(endpoint)
+
+
+get_response = requests.get(endpoint, json={"Query": "Hello world"})
 print(get_response.json())

937b7e5695a26cca47e320bb47de42e031637701
Jonathan Calabrese <Yonibrese@gmail.com>
2023-06-11 18:56:34 +0300

    finished basic GET request tutorial

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,12 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
 from products.models import products
 
 def api_home(request, *arg, **kwarg):
     model_data = products.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data["id"] = model_data.id
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
+       data = model_to_dict(model_data,fields=['id','title','price'])
     
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,3 +5,5 @@ endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"Query": "Hello world"})
 print(get_response.json())
+# print(get_response.headers)
+# print(get_response.text)
